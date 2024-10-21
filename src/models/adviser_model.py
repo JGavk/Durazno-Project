b2326a0e1894c78e88572288d237a2c7db3addc9
Juan Puerta <juan.pablo.puerta@correounivalle.edu.co>
2024-10-21 11:49:33 -0500

    little change

--- src/models/adviser_model.py
+++ src/models/adviser_model.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.hashers import make_password, check_password
 
 
 class Adviser(models.Model):
@@ -7,6 +9,13 @@ class Adviser(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=100)
     active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(default=timezone.now)
+
+    def set_password(self, password):
+        self.password = make_password(password)
+
+    def check_password(self, password):
+        return check_password(password, self.password)
 
     class Meta:
         db_table = 'adviser'
--- src/models/user_model.py
+++ src/models/user_model.py
@@ -11,11 +11,11 @@ class User(models.Model):
     password = models.CharField(max_length=250)
     is_active = models.BooleanField(default=True)
     last_login = models.DateTimeField(default=timezone.now)
+
     #is_admin = models.BooleanField(default=False)
     #is_superuser = models.BooleanField(default=False)
     #
     def set_password(self, password):
-        from django.contrib.auth.hashers import make_password
         self.password = make_password(password)
 
     def check_password(self, password):
