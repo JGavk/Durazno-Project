1b002a5bf1c482d02332b52b812b65522bb8d817
Juan Puerta <juan.pablo.puerta@correounivalle.edu.co>
2024-10-24 10:21:58 -0500

    adviser pers demo

---
+++ src/adviser/persistences/adviser_persistences.py
@@ -0,0 +1,19 @@
+from src.models import Adviser
+
+
+class AdviserPersistence:
+    def __init__(self):
+        self.adviser = Adviser()
+
+    def get_adviser(self):
+        return self.adviser
+
+    @staticmethod
+    def login_adviser(self, username, password):
+        try:
+            adviser = Adviser.objects.get(username=username)
+            if adviser.check_password(password):
+                return adviser
+
+        except Adviser.DoesNotExist:
+            return "Adviser does not exist"
