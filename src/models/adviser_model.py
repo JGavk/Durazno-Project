477dd9cd1872fbab3a683b9a3610882f464fc083
YeimerrrM <yeimer.mendez@correounivalle.edu.co>
2024-11-02 16:16:36 -0500

    Added adviser permissiones over canines

--- src/models/adviser_model.py
+++ src/models/adviser_model.py
@@ -1,25 +1,27 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.models import AbstractBaseUser
 
 
-class Adviser(models.Model):
+class Adviser(AbstractBaseUser):
     picture = models.ImageField(upload_to="adviser_pictures", default=False)
     name = models.CharField(max_length=100)
     email = models.EmailField()
     password = models.CharField(max_length=100)
     active = models.BooleanField(default=True)
     last_login = models.DateTimeField(default=timezone.now)
-
-    def set_password(self, password):
-        self.password = make_password(password)
-        self.save()
-
-    def check_password(self, password):
-        return check_password(password, self.password)
-
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'password', 'active',]
+    
     class Meta:
         db_table = 'adviser'
+        permissions = [
+                ("view_canine_model", "Can view canine model"),
+                ("add_canine_model", "Can add canine model"),
+                ("change_canine_model", "Can change canine model"),
+                ("delete_canine_model", "Can delete canine model"),
+            ]
         
     def __str__(self):
         return f'Adviser: {self.name}'
