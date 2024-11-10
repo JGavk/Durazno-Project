c1561bb2605624af6c6f7c663a1a6c7c565c2ab7
Soto Espitia <sotomilos004@gmail.com>
2024-11-10 15:30:15 -0500

    Agregacion de nuevos campos al login (falta organizarlos)

--- src/Views/Login/LoginView.tsx
+++ src/Views/Login/LoginView.tsx
@@ -5,25 +5,23 @@ import { useState } from 'react';
 import './LoginView.css';
 
 function LoginView() {
-  const [isLogin, setIsLogin] = useState(true); // Estado para alternar entre login y registro
-  const [showVerification, setShowVerification] = useState(false); // Estado para mostrar la verificación
-  const [verificationCode, setVerificationCode] = useState(''); // Estado para el código de verificación
+  const [isLogin, setIsLogin] = useState(true);
+  const [showVerification, setShowVerification] = useState(false);
+  const [verificationCode, setVerificationCode] = useState('');
 
   const toggleForm = () => {
-    setIsLogin(!isLogin); // Cambia entre login y registro
+    setIsLogin(!isLogin);
   };
 
-  // Función para manejar el registro
   const handleSignup = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setShowVerification(true);
   };
 
-  // Función para manejar la verificación del código
   const handleVerify = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (verificationCode.length === 4) {
-      setIsLogin(true); // Volver a la pantalla de inicio de sesión
+      setIsLogin(true);
       setShowVerification(false);
     } else {
       alert('Por favor, ingrese un código de 4 dígitos');
@@ -33,7 +31,6 @@ function LoginView() {
   return (
     <div className="login-container">
       <div className="login-card">
-        {/* Clase condicional para el logo basado en el estado */}
         <div className={`logo ${isLogin ? 'logo-login' : showVerification ? 'logo-verification' : 'logo-signup'}`}></div>
 
         <div className="header">
@@ -44,42 +41,35 @@ function LoginView() {
 
         {isLogin ? (
           <form autoComplete="off">
-            {/* // Input fantasma para el autoComplete off */}
             <input type="text" style={{ display: 'none' }}/> 
-
             <label>Email</label>
             <input type="email" placeholder="yeimc@gmail.com" required />
-
             <label>Contraseña</label>
             <input type="password" placeholder="******" required />
-
             <a href="#" className="forgot-password">¿Olvidaste tu contraseña?</a>
-
             <button type="submit">Iniciar sesión</button>
           </form>
         ) : showVerification ? (
-          // Formulario de verificación
           <form className="form-verification" onSubmit={handleVerify} autoComplete="off">
             <label>Ingresa el código de 4 dígitos que hemos enviado a tu correo</label>
             <input
               type="text"
               value={verificationCode}
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => setVerificationCode(e.target.value)}
-              maxLength={4} // Máximo de 4 caracteres
-              inputMode="numeric"  // Sugerencia para mostrar el teclado numérico en móviles
-              pattern="\d*"  // Acepta solo dígitos
+              maxLength={4}
+              inputMode="numeric"
+              pattern="\d*"
               required
             />
             <button type="submit">Verificar</button>
           </form>
         ) : (
-          // Formulario de registro
           <form className="form-signup" onSubmit={handleSignup} autoComplete="off">
             <label>Nombre de usuario</label>
             <input
               type="text"
               placeholder="Nombre de usuario"
-              pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+" // Solo letras
+              pattern="[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+"
               title="El nombre de usuario solo debe contener letras"
               required
             />
@@ -92,6 +82,23 @@ function LoginView() {
 
             <label>Confirmar Contraseña</label>
             <input type="password" placeholder="******" required />
+
+            {/* IMPORTANTE DE AQUI HASTA DIRECCION ORGANIZAR CODIGO */}
+            <label>Teléfono</label>
+            <input
+              type="tel"
+              placeholder="123-456-7890"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              title="Formato: 123-456-7890"
+              required
+            />
+
+            <label>Dirección</label>
+            <input
+              type="text"
+              placeholder="Dirección completa"
+              required
+            />
 
             <button type="submit">Registrarse</button>
           </form>
