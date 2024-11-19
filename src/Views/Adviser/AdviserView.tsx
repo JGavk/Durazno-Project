4a926fb7ee7ef0c5ead2a5ed29c3585bd6171c6d
YeimerrrM <yeimer.mendez@correounivalle.edu.co>
2024-11-19 16:00:04 -0500

    Add style to client and adviser view and add data

--- src/Views/Adviser/AdviserView.tsx
+++ src/Views/Adviser/AdviserView.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import './AdviserView.css';
 
 const AdviserView = () => {
+  interface Canine {
+    id: number;
+    name: string;
+    breed: string;
+    age: number;
+  }
+
+  const mockCanines: Canine[] = [
+      { id: 1, name: 'Rex', breed: 'German Shepherd', age: 5 },
+      { id: 2, name: 'Bella', breed: 'Labrador Retriever', age: 3 },
+      { id: 3, name: 'Max', breed: 'Bulldog', age: 4 },
+  ];
     return (
         <div>
-            <h1>AdviserView</h1>
+            <h1>Manage Canines</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Breed</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {mockCanines.map((canine) => (
+                        <tr key={canine.id}>
+                            <td>{canine.id}</td>
+                            <td>{canine.name}</td>
+                            <td>{canine.breed}</td>
+                            <td>{canine.age}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
-}
+};
 
 export default AdviserView;
--- src/Views/Client/ClientView.tsx
+++ src/Views/Client/ClientView.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { getUserById, logoutUser } from '../services/authRoutes';
+import './ClientView.css';
 
 const ClientView: React.FC = () => {
 
